a4b282d7fe772bf9833dfab3ecc61bf0cb21bc7f
EbrahimAlipour <mohammadebrahimalipour1377@gmail.com>
2022-05-06 05:58:19 +0430

    fix SupplierSpecific Create feature ( add actions, validate and onSuccess)

--- src/SupplierSpecific/SupplierSpecificCreate.js
+++ src/SupplierSpecific/SupplierSpecificCreate.js
@@ -2,18 +2,52 @@ import * as React from "react";
 import {
     SimpleForm,
     TextInput,
-    Create
+    Create,
+    TopToolbar,
+    ListButton,
+    useNotify,
+    useRefresh,
+    useRedirect
 }
-from 'react-admin';
+    from 'react-admin';
 
-const SupplierSpecificCreate = props => (
-    <Create {...props} title="ایجاد ویژگی تامین کننده">
-        <SimpleForm>
-            <TextInput label="کد ویژگی تامین کننده" textAlgin="right" source="SupplierSpecificCode" />
-            <TextInput label="نام ویژگی تامین کننده" textAlgin="right" source="SupplierSpecificName" />
-        </SimpleForm>
-    </Create>
+const CreateActions = ({ basePath}) => (
+    <TopToolbar>
+        <ListButton basePath={basePath} />
+    </TopToolbar>
 );
+
+const validateError = (values) => {
+    const errors = {};
+    if (!values.SupplierSpecificCode) {
+        errors.SupplierSpecificCode = 'کد را وارد کنید';
+    }
+    if (!values.SupplierSpecificName) {
+        errors.SupplierSpecificName = 'نام را وارد کنید';
+    }
+    return errors
+};
+
+
+const SupplierSpecificCreate = props => {
+    const notify = useNotify();
+    const refresh = useRefresh();
+    const redirect = useRedirect();
+
+    const onSuccess = () => {
+        notify(`دیتا ذخیره شد`)
+        redirect('/PMWorks/SupplierSpecific/create');
+        refresh();
+    };
+    return(
+        <Create actions={<CreateActions />} onSuccess={onSuccess}  {...props} title="ایجاد ویژگی تامین کننده">
+            <SimpleForm validate={validateError}>
+                <TextInput label="کد ویژگی تامین کننده" textAlgin="right" source="SupplierSpecificCode"/>
+                <TextInput label="نام ویژگی تامین کننده" textAlgin="right" source="SupplierSpecificName"/>
+            </SimpleForm>
+        </Create>
+    )
+};
 
 
 export default SupplierSpecificCreate;
